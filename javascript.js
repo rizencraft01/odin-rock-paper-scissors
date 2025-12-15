2c1f4661e690c3106a00cc01b20d7bb39f77da2f
ralphl22 <134184625+ralphl22@users.noreply.github.com>
2025-12-14 21:05:32 -0500

    Change how game is played & update files

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
 function getComputerChoice(choice = Math.floor(Math.random() * 3)) {
     if (choice == 0) {
-        console.log('Computer: Rock')
+        let message = document.createElement('p')
+
+        message.textContent = 'Computer Choice: Rock'
+
+        results.appendChild(message)
+        
         return 'Rock'
     }
     else if (choice == 1) {
-        console.log('Computer: Scissors')
+        message = document.createElement('p')
+
+        message.textContent = 'Computer Choice: Scissors'
+
+        results.appendChild(message)
+
         return 'Scissors'
     }
     else if (choice == 2) {
-        console.log('Computer: Paper')
+        message = document.createElement('p')
+
+        message.textContent = 'Computer Choice: Paper'
+
+        results.appendChild(message)
+
         return 'Paper'
     }
 }
@@ -19,58 +34,187 @@ function getHumanChoice(humanChoice = prompt('Choose rock, paper, or scissors:')
 
     if (humanChoice == 'rock') {
         console.log('-----------------------------------------')
-        console.log('Player: Rock')
+        console.log('Player Choice: Rock')
         return 'Rock'
     }
     else if (humanChoice == 'paper') {
         console.log('-----------------------------------------')
-        console.log('Player: Paper')
+        console.log('Player Choice: Paper')
         return 'Paper'
     }
     else if (humanChoice == 'scissors') {
         console.log('-----------------------------------------')
-        console.log('Player: Scissors')
+        console.log('Player  Choice: Scissors')
         return 'Scissors'
     }
 }
 
 function playGame() {
     let humanScore = 0
+
     let computerScore = 0
 
-    while (humanScore < 5 && computerScore < 5) {
-        function playRound(humanChoice = getHumanChoice(), computerChoice = getComputerChoice()) {
-            if (humanScore < 5 && computerScore < 5) {
-                if (humanChoice == 'Rock' && computerChoice == 'Paper' || humanChoice == 'Paper' && computerChoice == 'Scissors' || humanChoice == 'Scissors' && computerChoice == 'Rock') {
-                    console.log(`The computer scores! ${computerChoice} beats ${humanChoice}!`)
-                    console.log('-----------------------------------------')
-                    computerScore++
-                }
-                else if (humanChoice == 'Rock' && computerChoice == 'Scissors' || humanChoice == 'Paper' && computerChoice == 'Rock' || humanChoice == 'Scissors' && computerChoice == 'Paper') {
-                    console.log(`The player scores! ${humanChoice} beats ${computerChoice}!`)
-                    console.log('-----------------------------------------')
-                    humanScore++
-                }
-                else if (humanChoice == computerChoice) {
-                    console.log(`It's a tie!`)
-                    console.log('-----------------------------------------')
-                }
-            }
-            if (computerScore == 5) {
-                console.log('-----------------------------------------')
-                console.log(`The computer wins the game! ${computerChoice} beats ${humanChoice}!`)
-                console.log('-----------------------------------------')
-            }
-            else if (humanScore == 5) {
-                console.log('-----------------------------------------')
-                console.log(`The player wins the game! ${humanChoice} beats ${computerChoice}!`)
-                console.log('-----------------------------------------')
-            }
+    let message;
+
+    let message2;
+
+    let displayHumanScore;
+
+    let displayComputerScore;
+
+    const selectRockButton = document.querySelector('#rock-button')
+
+    const selectScissorsButton = document.querySelector('#scissors-button')
+
+    const selectPaperButton = document.querySelector('#paper-button')
+
+    const results = document.querySelector("#results")
+    
+    selectRockButton.addEventListener('click', () => {
+        if (humanScore == 5 || computerScore == 5) return false
+
+        message = document.createElement('p')
+
+        message.textContent = '-----------------------------------------'
+
+        message2 = document.createElement('p')
+
+        message2.textContent = 'Player Choice: Rock'
+
+        results.appendChild(message)
+
+        results.appendChild(message2)
+        
+        return playRound('Rock')
+    })
+
+    selectPaperButton.addEventListener('click', () => {
+        if (humanScore == 5 || computerScore == 5) return false
+
+        message = document.createElement('p')
+
+        message.textContent = '-----------------------------------------'
+
+        message2 = document.createElement('p')
+
+        message2.textContent = 'Player Choice: Paper'
+
+        results.appendChild(message)
+
+        results.appendChild(message2)
+
+        return playRound('Paper')
+    })
+
+    selectScissorsButton.addEventListener('click', () => {
+        if (humanScore == 5 || computerScore == 5) return false
+
+        message = document.createElement('p')
+
+        message.textContent = '-----------------------------------------'
+
+        message2 = document.createElement('p')
+
+        message2.textContent = 'Player Choice: Scissors'
+
+        results.appendChild(message)
+        
+        results.appendChild(message2)
+
+        return playRound('Scissors')
+    })
+
+    function playRound(humanChoice, computerChoice = getComputerChoice()) {
+        if (humanChoice == 'Rock' && computerChoice == 'Paper' || humanChoice == 'Paper' && computerChoice == 'Scissors' || humanChoice == 'Scissors' && computerChoice == 'Rock') {   
+            computerScore++
+            
+            displayHumanScore = document.createElement('p')
+
+            displayComputerScore = document.createElement('p')
+
+            displayHumanScore.textContent = `Player Score: ${humanScore}`
+
+            displayComputerScore.textContent = `Computer Score: ${computerScore}`
+            
+            message = document.createElement('p')
+
+            humanChoice = humanChoice.toLowerCase()
+
+            if (computerScore == 5) message.textContent = `The computer wins the game! ${computerChoice} beats ${humanChoice}!`
+            else message.textContent =`The computer scores! ${computerChoice} beats ${humanChoice}!`
+
+            message2 = document.createElement('p')
+
+            message2.textContent = '-----------------------------------------'
+
+            results.appendChild(message)
+
+            results.appendChild(displayHumanScore)
+
+            results.appendChild(displayComputerScore)
+
+            results.appendChild(message2)
+
         }
+        else if (humanChoice == 'Rock' && computerChoice == 'Scissors' || humanChoice == 'Paper' && computerChoice == 'Rock' || humanChoice == 'Scissors' && computerChoice == 'Paper') {
+            humanScore++
 
-        playRound()
+            displayHumanScore = document.createElement('p')
 
+            displayComputerScore = document.createElement('p')
+
+            displayHumanScore.textContent = `Player Score: ${humanScore}`
+
+            displayComputerScore.textContent = `Computer Score: ${computerScore}`
+            
+            message = document.createElement('p')
+
+            computerChoice = computerChoice.toLowerCase()
+
+            if (humanScore == 5) message.textContent = `The player wins the game! ${humanChoice} beats ${computerChoice}!`
+            else message.textContent =`The player scores! ${humanChoice} beats ${computerChoice}!`
+
+            message2 = document.createElement('p')
+
+            message2.textContent = '-----------------------------------------'
+
+            results.appendChild(message)
+
+            results.appendChild(displayHumanScore)
+
+            results.appendChild(displayComputerScore)
+
+            results.appendChild(message2)
+
+        }
+        else if (humanChoice == computerChoice) {
+            displayHumanScore = document.createElement('p')
+
+            displayComputerScore = document.createElement('p')
+
+            displayHumanScore.textContent = `Player Score: ${humanScore}`
+
+            displayComputerScore.textContent = `Computer Score: ${computerScore}`
+
+            message = document.createElement('p')
+
+            message.textContent =`It's a tie!`
+
+            message2 = document.createElement('p')
+
+            message2.textContent = '-----------------------------------------'
+
+            results.appendChild(message)
+
+            results.appendChild(displayHumanScore)
+
+            results.appendChild(displayComputerScore)
+
+            results.appendChild(message2)
+        }
     }
+    
 }
 
 playGame()
+
